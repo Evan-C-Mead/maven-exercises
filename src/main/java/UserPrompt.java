e5ae11a1f918e823c15a97580c224a2b74a96bd0
Evan Mead <evan.c.mead@gmail.com>
2021-01-08 10:02:00 -0600

    returns all three methods - working on command line messages

--- src/main/java/UserPrompt.java
+++ src/main/java/UserPrompt.java
@@ -1,5 +1,16 @@
 import java.util.*;
+import static org.apache.commons.lang3.StringUtils.*;
+
 
 public class UserPrompt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Type something...anything...");
+        String input = sc.nextLine();
+
+        System.out.println(isNumeric(input));
+        System.out.println(swapCase(input));
+        System.out.println(reverse(input));
+    }
 }
